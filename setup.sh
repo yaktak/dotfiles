c71b0ad4b44639e3be1dcb1598f5f59e8bd4faf3
yaktak <yakei034@gmail.com>
2018-01-29 16:17:22 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+CONFIG_DIR="$HOME/.config"
+if [ -e $CONFIG_DIR ]; then
+    mkdir $CONFIG_DIR
+    echo ".config directory created"
+fi
+
 DOT_FILES="$HOME/dotfiles"
 cd $DOT_FILES
 
-for f in .??*
-do
+for f in .??*; do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == "init.vim"]] && mv "$f" "$CONFIG_DIR" && continue
 
     ln -s "$DOT_FILES/$f" "$HOME/$f"
 done
