7e5c5a57d1ca54804598c6ac7755dccf0d1ea94e
yaktak <13303430+yaktak@users.noreply.github.com>
2019-01-21 14:43:07 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -ux
+cd `dirname $0`
+
 CONFIG_DIR="$HOME/.config"
 if [ ! -e $CONFIG_DIR ]; then
     mkdir $CONFIG_DIR
@@ -23,8 +26,20 @@ for f in *; do
     ln -s "$DOT_FILES/$f" "$HOME/$f"
 done
 
-if !(type "git" > /dev/null 2>&1); then
-    echo "git not found"
+if [ -n "$(command -v yum)" ]; then
+    sudo yum install -y epel-release
+    sudo yum install -y git wget vim automake
+elif [ -n "$(command -v apt)" ]; then
+    sudo apt update >/dev/null
+    sudo apt install -y git wget vim automake
 fi
+
+git clone https://github.com/tmux/tmux.git
+cd ./tmux
+sh autogen.sh
+./configure
+make
+sudo make install
+rm -rf ./tmux
 
 git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
